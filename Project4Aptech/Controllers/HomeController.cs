fec6f4b36785003fdf22782ff8c0f9eb95257ee6
tu huu <Jaryhuu@gmail.com>
2021-03-23 18:53:51 +0700

    them chuc nang dang nhap lan dau doi mk

--- Project4Aptech/Controllers/HomeController.cs
+++ Project4Aptech/Controllers/HomeController.cs
@@ -63,10 +63,12 @@ namespace Project4Aptech.Controllers
                 {
                     ViewBag.err = "Your account has been locked due to some reasons,please contact our staff for more information";
                     return View();
-                }else if (isValid.A_Status==0)
+                }
+                else if (isValid.A_Status==0)
                 {
-                    ViewBag.err = "You are logging for the 1st time,pls change your password for security purpose ";
-                    return View();
+                    TempData["usnId"] = isValid.id;
+                    TempData["err"] = "Please change your password as this is the first time ";
+                    return RedirectToAction("ChangePwd");
                 }
                 Session["logged"] = isValid;
                 return RedirectToAction("Index");
@@ -79,7 +81,62 @@ namespace Project4Aptech.Controllers
                 return View();
             }
         }
-
+        public ActionResult ChangePwd()
+        {
+            ViewBag.usnid = TempData["usnId"];
+            if (TempData["err"] != null)
+            {
+                ViewBag.err = TempData["err"];
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChangePwd(int usnId,string newpass,string repass )
+        {
+            char[] nums = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            bool containNm = false;
+            foreach (var i in newpass)
+            {
+                if (nums.Contains(i))
+                {
+                    containNm = true;
+                    break;
+                }
+            }
+            if (TempData["err"] != null)
+            {
+                ViewBag.err = TempData["err"];
+            }
+            ViewBag.usnid = usnId;
+            ViewBag.pass = newpass;
+            ViewBag.re = repass;
+            if (newpass != repass)
+            {
+                ViewBag.err = "Re-enter incorrectly";
+                return View();
+            }else if (string.IsNullOrEmpty(newpass) || string.IsNullOrEmpty(repass))
+            {
+                ViewBag.err = "Please fill all the field";
+                return View();
+            }
+            else if (newpass.Count() <8 || newpass.Count() > 40)
+            {
+                ViewBag.err = "Your password must be 8-40-characters ";
+                return View();
+            }else if (!containNm)
+            {
+                ViewBag.err = "Your password must contain number";
+                return View();
+            }
+            else
+            {
+                var isvalid = db.Account.Find(usnId);
+                isvalid.Pwd = r.HashPwd(newpass);
+                isvalid.A_Status = 1;
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
